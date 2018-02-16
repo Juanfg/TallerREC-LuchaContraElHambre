7cd0afd0b28bf1e7990b6f6acf3005a4be736a18
Juan Pablo Flores Galindo <juanfg@outlook.com>
2018-02-15 23:11:44 -0600

    Fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 8085));
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
